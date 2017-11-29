14ad416685549a054db8e086042c1ef1b3c270ee
sunxinxin <nothsun@163.com>
2017-11-29 16:38:40 +0800

    Add annotation code in utils

--- practices/test_parse_home_dir.py
+++ practices/test_parse_home_dir.py
@@ -1,6 +1,9 @@
-from unittest import TestCase
+from unittest import TestCase, main
 from utils import parse_home_dir
 
 class TestParse_home_dir(TestCase):
     def test_parse_home_dir(self):
         self.assertEqual(parse_home_dir('log', 'error.log'), r'C:\Users\Admin\log\error.log', "Not expected result")
+
+if __name__ == '__main__':
+    main()
