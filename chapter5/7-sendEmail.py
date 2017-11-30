4186ccfbdade9bcc6014014b1e80ddeb5cd57372
sunxinxin <sunxinxin@83chedai.com>
2017-11-30 16:51:32 +0800

    Annotate email sending

--- chapter5/7-sendEmail.py
+++ chapter5/7-sendEmail.py
@@ -7,6 +7,6 @@ msg['Subject'] = "An Email Alert"
 msg['From'] = "ryan@pythonscraping.com"
 msg['To'] = "webmaster@pythonscraping.com"
 
-s = smtplib.SMTP('localhost')
+s = smtplib.SMTP('localhost')  # localhost can be replaced by remote email client address, like smtp.xxx.com
 s.send_message(msg)
 s.quit()
